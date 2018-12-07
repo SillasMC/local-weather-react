d8cf9dbee2ab5e44f52ac0b9231084590ceb3266
Sillas Camara <sillasmc@gmail.com>
2018-12-06 23:04:41 -0300

    Get static weather data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Weather from './Weather';
 
 class App extends Component {
-	state = {
-		data: []
-	};
+	constructor(props) {
+		super(props);
 
-	// componentDidMount() {
-	// 	const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
-	//
-	// 	fetch(url)
-	// 		.then(result => result.json())
-	// 		.then(result => {
-	// 			this.setState({
-	// 				data: result
-	// 			})
-	// 		});
-	// }
+		this.initialState = {
+			name: '',
+			country: '',
+			temp: 0,
+			tempScale: 'C',
+			weather: '',
+			weatherDesc: 'image',
+			icon: ''
+		};
+
+		this.state = this.initialState;
+	}
+
+	componentDidMount() {
+		const url = 'https://fcc-weather-api.glitch.me/api/current?lat=-3.7650466999999996&lon=-38.5577689';
+
+		axios.get(url)
+			.then(({data}) => {
+				var standardData = {
+					name: data.name,
+					country: data.sys.country,
+					temp: data.main.temp,
+					tempScale: 'C',
+					weather: data.weather[0].main,
+					weatherDesc: data.weather[0].description,
+					icon: data.weather[0].icon
+				};
+				this.setState(standardData);
+			})
+			.catch(error => console.error(error));
+	}
 
 	render() {
 		return (
 			<div className="main-div">
 				{/* TODO: Navbar on Future*/}
 				<p className="temp-row">Local Weather App  <i className="pl10 fa fa-cloud"></i></p>
-				<Weather />
+				<Weather weatherData={this.state} />
 				{/* TODO: Author Footer*/}
 			</div>
 		);
--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
 
 class Weather extends Component {
-	constructor(props) {
-		super(props);
-
-		this.initialState = {
-			name: '',
-			country: '',
-			temp: 0,
-			tempScale: 'C',
-			weather: '',
-			weatherDesc: 'image',
-			icon: 'https://www.w3schools.com/w3css/img_lights.jpg'
-		};
-
-		this.state = this.initialState;
-	}
-
 	render() {
-		const { name, country, temp, tempScale, weather, weatherDesc, icon } = this.state;
+		const { name, country, temp, tempScale, weather, weatherDesc, icon } = this.props.weatherData;
 
 		return (
 			<div className="weather-row">
