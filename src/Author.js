a8700d4f8768961e01c62105eaf9c5a747648d1d
Sillas Camara <sillasmc@gmail.com>
2018-12-26 22:12:54 -0300

    FIX: target blank security issue

--- src/Author.js
+++ src/Author.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Author = () => {
 	return (
 		<div className="code-author">
-			<p>Developed and designed by <a target='_blank' href="https://github.com/SillasMC">Sillas</a></p>
+			<p>Developed and designed by <a target='_blank' rel="noopener noreferrer" href="https://github.com/SillasMC">Sillas</a></p>
 		</div>
 	);
 }
